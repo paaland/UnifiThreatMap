94d44e055ce92c59fab30baab0097a9c92baa769
Pål Andreassen <paal.andreassen@gmail.com>
2022-07-09 21:46:46 +0200

    Mintor update

--- docker_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop unifithreatmap 
-docker rm unifithreatmap
-docker run -d --name unifithreatmap -p 9999:9999 unifithreatmap:latest
-docker logs -f unifithreatmap
---
+++ docker_run_local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker stop unifithreatmap 
+docker rm unifithreatmap
+docker run -d --name unifithreatmap \
+       -p 9999:9999 \
+       -e unifi_ip=<UnifiLanIp> \
+       -e unifi_username=<UnifiLocalAdminUserName> \
+       -e unifi_password='<UnifiLocalAdminPassword>' \
+       paaland/unifithreatmap:latest
+docker logs -f unifithreatmap
